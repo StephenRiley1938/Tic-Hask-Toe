26f40001733e4fa912ba7e2d4cc3b7dc03ff827a
Steve Riley <riley@umich.edu>
2023-05-13 20:50:21 -0400

    Finished A5.

--- solutions/A5.hs
+++ solutions/A5.hs
@@ -11,45 +11,93 @@ import Control.Monad (when)
 -- *** Assignment 5-1 *** --
 
 -- Q#01
-
-printBoard = undefined
+printBoard :: Board -> IO ()
+printBoard board = putStrLn (formatBoard board)
 
 -- Q#02
 _LOGO_PATH_ :: FilePath
 _LOGO_PATH_ = "./assets/logo.txt"
 
-
-printLogo = undefined
+printLogo :: IO ()
+printLogo = readFile _LOGO_PATH_ >>= putStrLn
 
 -- Q#03
 _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
-
-firstPlayer = undefined
+firstPlayer :: IO Player
+firstPlayer = _RANDOM_BOOL_ >>= (\p -> (return . getFirstPlayer) p)
 
 -- Q#04
 
-getMove = undefined
+getMove :: Board -> IO Move
+getMove board = getLine >>= (\line -> let move = stringToMove line
+    in  if (move == _INVALID_MOVE_) || not (isValidMove board move)
+            then putStrLn "Invalid move! Try again" >> getMove board
+        else
+            return move)
 
 -- Q#05
 
-play = undefined
+play :: Board -> Player -> IO ()
+play board player =
+    when _DISPLAY_LOGO_ printLogo >>
+    printBoard board >>
+    print (promptPlayer player) >>
+    getMove board >>= (\move -> let (gamestate, newboard) = playMove player board move
+        in  if gamestate == GameInProgress
+                then play newboard (switchPlayer player)
+            else
+                printBoard newboard >>
+                putStrLn (showGameState gamestate) >>
+                return ()
+            )
+
+
 
 -- *** Assignment 5-2 *** --
 
 -- Q#07
 
-printLogoDo = undefined
+printLogoDo :: IO ()
+printLogoDo = do
+    lf <- readFile _LOGO_PATH_
+    putStrLn lf
 
 -- Q#08
 
-firstPlayerDo = undefined
+firstPlayerDo :: IO Player
+firstPlayerDo = do
+    rb <- _RANDOM_BOOL_
+    (return . getFirstPlayer) rb
 
 -- Q#09
 
-getMoveDo = undefined
+getMoveDo :: Board -> IO Move
+getMoveDo board = do
+    line <- getLine
+    let move = stringToMove line
+    if (move == _INVALID_MOVE_) || not (isValidMove board move)
+        then do
+            putStrLn "Invalid move! Try again"
+            getMove board
+        else do
+            return move
 
 -- Q#10
 
-playDo = undefined
+playDo :: Board -> Player -> IO ()
+playDo board player = do
+    when _DISPLAY_LOGO_ printLogo
+    printBoard board
+    print (promptPlayer player)
+    move <- getMove board
+    let (gamestate, newboard) = playMove player board move
+    if gamestate == GameInProgress
+        then do
+            play newboard (switchPlayer player)
+        else do
+            printBoard newboard
+            putStrLn (showGameState gamestate)
+            return ()
+            
--- solutions/Main.hs
+++ solutions/Main.hs
@@ -7,4 +7,12 @@ import A4
 import A5
 
 main :: IO ()
-main = putStrLn "Welcome to Part I of EMURGO Academy's Haskell course!"
+--main = putStrLn "Welcome to Part I of EMURGO Academy's Haskell course!"
+
+{-
+main = let doplay = play _EMPTY_BOARD_
+    in firstPlayer >>= doplay
+-}
+
+main = let doplay = playDo _EMPTY_BOARD_
+    in firstPlayer >>= doplay    
